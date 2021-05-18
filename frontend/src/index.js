739f1f327e6e0cc15e9504b634bbdc81f3286bcf
thedanielleellis <thedanielleellis@gmail.com>
2021-05-18 11:50:07 -0500

    created getMentors event and mentor form html

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,9 +1,24 @@
 const endPoint = ("http://localhost:3000/api/v1/mentors")
 
 document.addEventListener('DOMContentLoaded', () => {
-    fetch (endPoint)
-    .then(response => response.json())
-    .then(mentor => {
-        console.log(mentor);
-    })
+    getMentors()
 })
+
+function getMentors() {
+    fetch(endPoint)
+        .then(res => res.json())
+        .then(mentors => {
+            mentors.data.forEach(mentor => {
+                const mentorMarkup =
+                    <div data-id=${mentor.id}>
+                        <img src=${mentor.attributes.image_url} height="200" width="250">
+                            <h3>${mentor.attributes.title}</h3>
+                            <p>${mentor.attributes.department.name}</p>
+                            <button data-id=${mentor.id}>edit</button>
+                    </div>
+                        <br></br>
+
+            document.querySelector('#mentor-container').innerHTML+= mentorMarkup
+            })
+        })
+}
