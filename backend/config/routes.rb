e333eddf4d2ee5a39018e7b6783271fe9ee70e49
thedanielleellis <thedanielleellis@gmail.com>
2021-05-17 22:40:48 -0500

    created routes

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  resources :mentors
-  resources :departments
+  namespace :api do
+    namespace :v1 do
+      resources :mentors, only: [:index, :create]
+      resources :departments,only: [:index]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+    end
+  end
 end
