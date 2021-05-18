cf1f9e7b2aba3532c9a00e6a433358b26b6df72b
thedanielleellis <thedanielleellis@gmail.com>
2021-05-17 22:32:47 -0500

    updated seed with destroy.all

--- backend/app/models/department.rb
+++ backend/app/models/department.rb
@@ -1,3 +1,3 @@
 class Department < ApplicationRecord
-    has_many :mentors
+    has_many :mentors, dependent: :destroy
 end
--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -6,8 +6,11 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+Department.destroy.all
+Mentor.destroy.all 
+
 tech = Department.create(name: "Technology")
 admin = Department.create(name: "Administration")
 operations = Department.create(name: "Operations") 
 
-Mentor.create(name: "Olivia Pope", title: "Operations Manager", bio: "I have been working at the company 20 years and love spotting rising talent", image_url: "https://i.insider.com/5b9a970d3cccd127008b45cd?width=700", department_id: operations.id)
+Mentor.create(name: "Olivia Pope", title: "Operations Manager", about: "I have been working at the company 20 years and love spotting rising talent", image_url: "https://i.insider.com/5b9a970d3cccd127008b45cd?width=700", department_id: operations.id)
