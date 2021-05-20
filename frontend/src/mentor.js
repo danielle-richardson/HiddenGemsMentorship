e417d26b0d9240fca3c4cbbf968ab6af32b6433a
thedanielleellis <thedanielleellis@gmail.com>
2021-05-20 01:44:06 -0500

    fixed images to grid style

--- frontend/src/mentor.js
+++ frontend/src/mentor.js
@@ -1,44 +1,32 @@
 class Mentor {
-    constructor(mentor, mentorAttributes) {
-        this.id = mentor.id
-        this.name = mentorAttributes.name
-        this.title = mentorAttributes.title
-        this.about = mentorAttributes.about
-        this.image_url = mentorAttributes.image_url
-        this.department = mentorAttributes.department
-        Mentor.all.push(this)
-        console.log(this);
-    }
+  constructor(mentor, mentorAttributes) {
+    this.id = mentor.id
+    this.name = mentorAttributes.name
+    this.title = mentorAttributes.title
+    this.about = mentorAttributes.about
+    this.image_url = mentorAttributes.image_url
+    this.department = mentorAttributes.department
+    Mentor.all.push(this)
+    console.log(this);
+  }
 
-    renderMentorCard() {
-        return `
-      <div class="col-md-4">
-        <div class="card mb-4 shadow-sm">
-          <img src=${this.image_url} class="card-img-top" alt="...">
-          <div class="card-body">
-            <h5 class="card-title">${this.title}</h5>
-            <p class="card-text">${this.about}</p>
-            <div class="d-flex justify-content-between align-items-center">
+  renderMentorCard() {
+    return `
+        <div class="mentorlist">
+          <div class="row">
+            <div class="column">
+              <img src=${this.image_url} style="width:100%">
+              <h5>${this.title}</h5>
+              <h5>Department: ${this.department.name}</h5>
+              <p>${this.about}</p>
               <div class="btn-group">
-                <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
-                <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
-              </div>
-              <small class="text-muted">Department: ${this.department.name}</small>
+                <button type="button" class="btn btn">View</button>
+                <button type="button" class="btn btn">Edit</button>
+              </div> 
             </div>
-          </div>
-        </div>
-      </div>
-`
-
-// return `
-//        < div data - id=${ mentor.id }>
-//        <img src=${mentor.attributes.image_url} height="200" width="250">
-//        <h3>${mentor.attributes.title}</h3>
-//        <p>${mentor.attributes.depsartment.name}</p>
-//        <button data-id=${mentor.id}>edit</button>
-//    </div>
-//    <br></br>`;
-    }
+          <div>
+        </div> <br> `
+  }
 }
 
 Mentor.all = [];
