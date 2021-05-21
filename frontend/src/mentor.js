c966c7d2691e12663886b411e9ecba2ac9557af6
thedanielleellis <thedanielleellis@gmail.com>
2021-05-21 13:45:39 -0500

    updated html

--- frontend/src/mentor.js
+++ frontend/src/mentor.js
@@ -11,22 +11,28 @@ class Mentor {
   }
 
   renderMentorCard() {
-    return `
-        <div class="mentorlist">
-          <div class="row">
-            <div class="column">
-              <img src=${this.image_url} style="width:100%">
-              <h5>${this.title}</h5>
-              <h5>Department: ${this.department.name}</h5>
-              <p>${this.about}</p>
-              <div class="btn-group">
-                <button type="button" class="btn btn">View</button>
-                <button type="button" class="btn btn">Edit</button>
-              </div> 
-            </div>
-          <div>
-        </div> <br> `
+   return ` 
+    <div class="col-md-4">
+      <div class="card mb-4 shadow-sm">
+        <img src=${this.image_url} class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">${this.title}</h5>
+          <p class="card-text">${this.about}</p>
+          <div class="d-flex justify-content-between align-items-center">
+          <div class="btn-group">
+            <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
+            <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
+          </div>
+          <small class="text-muted">Department: ${this.department.name}</small>
+        </div>
+      </div>
+     </div>
+     </div>
+     </div>
+     </div>
+    </div> ` 
   }
+
 }
 
 Mentor.all = [];
